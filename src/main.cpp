2124c4688dacabbb621122bda3c19fa86230bf74
sina nojavan <sina.nojavan.c@gmail.com>
2025-11-24 12:03:33 +0330

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,21 @@ private:
 public:
     Perceptron(int inputsize,float lr){
         learningrate = lr;
-        weight.resize(inputsize);
+        weight.assign(inputsize, 0.0f);
+        bias = 0.0f;
     }
     ~Perceptron(){ weight.clear(); }
 
-    float predict(vector<float> inputs){
+    bool predict(const vector<float>& inputs){
         float sum = bias;
         for (int i = 0; i < inputs.size(); i++)
             sum += weight[i] * inputs[i];
         
-        return sum;
+        return sum >= 0;
     }
 
-    void train( vector<vector<float>> inputs,
-                vector<int> label,
+    void train(const vector<vector<float>>& inputs,
+               const vector<int>& label,
                 int epoch)
     {
         for (int k = 0; k < epoch; k++){
@@ -34,8 +35,8 @@ public:
                 int error = label[i] - prediction;
 
                 // update weight
-                for (int j = 0; j < inputs.size(); j++){
-                    weight[i] += learningrate * error * inputs[i][j];
+                for (int j = 0; j < weight.size(); j++){
+                    weight[j] += learningrate * error * inputs[i][j];
                 }
                 bias += learningrate * error;
             }
@@ -46,24 +47,20 @@ public:
 
 int main(){
 
-    Perceptron nuron(2,0.1);
+    Perceptron nuron(2,0.1f);
     vector<vector<float>> inputs = {
         {0,0},  {0,1},
-        {1,0},  {1,1},
+        {1,0},  
     };
     vector<int> label = {
         0,  0,
-        1,  1,
-    }
+        1,  
+    };
 
-    nuron.train(inputs,label,0.1);
-    cout << "\n (0,0): (P,R) (" << norun.predict({0,0})
-        <<  " , " << label[0];
-    cout << "\n (0,1): (P,R) (" << norun.predict({0,1})
-        <<  " , " << label[1];
-    cout << "\n (1,0): (P,R) (" << norun.predict({1,0})
-        <<  " , " << label[2];
-    cout << "\n (1,1): (P,R) (" << norun.predict({1,1})
-        <<  " , " << label[3];
+    nuron.train(inputs,label,10);
+    cout << "\n (0,0): (P,R) (" << (nuron.predict({0,0}) ? 1 : 0) <<  " , " << label[0] << ")";
+    cout << "\n (0,1): (P,R) (" << (nuron.predict({0,1}) ? 1 : 0) <<  " , " << label[1] << ")";
+    cout << "\n (1,0): (P,R) (" << (nuron.predict({1,0}) ? 1 : 0) <<  " , " << label[2] << ")";
+    cout << "\n (1,1): (P,R) (" << (nuron.predict({1,1}) ? 1 : 0) <<  " , "  << ")";
     return 0;
 }
