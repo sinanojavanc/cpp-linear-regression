d4d157473affef707b3d85e832238bdae94443c3
sina nojavan <sina.nojavan.c@gmail.com>
2025-11-24 13:53:15 +0330

    .

--- src/main.cpp
+++ src/main.cpp
@@ -28,18 +28,25 @@ public:
     void train(const vector<vector<float>>& inputs,
                const vector<int>& label,
                 int epoch)
-    {
+    {   // need to normalize the inpuuts
         for (int k = 0; k < epoch; k++){
+            double mse = 0.0;
+
             for (int i = 0; i < inputs.size(); i++){
                 int prediction = predict(inputs[i]);
                 int error = label[i] - prediction;
+                mse += error * error; // mse sum
 
+                // need to be curve lr
                 // update weight
                 for (int j = 0; j < weight.size(); j++){
                     weight[j] += learningrate * error * inputs[i][j];
                 }
                 bias += learningrate * error;
             }
+            // calculate the average and print for each epoch
+            mse /= inputs.size(); 
+            cout << "Epoch " << k+1 << " MSE: " << mse << endl;
         }
     }
 
