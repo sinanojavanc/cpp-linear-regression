5dfe4eeb9860f000b8bd5152c202c154af3effa3
sina nojavan <sina.nojavan.c@gmail.com>
2025-11-25 22:46:38 +0330

    curve the learning rate and command

--- src/perceptron.cpp
+++ src/perceptron.cpp
@@ -24,22 +24,27 @@ bool Perceptron::predict(const vector<float>& inputs) {
 
 void Perceptron::train(const vector<vector<float>>& inputs,
                        const vector<int>& label,
-                       int epoch)
+                       int epoch)                   // need to normalize the inpuuts
 {
     for (int k = 0; k < epoch; k++) {
         double mse = 0.0;
+        double lr  = learningrate * epoch;
+        // need to be curve lr 
 
         for (int i = 0; i < inputs.size(); i++) {
             int prediction = predict(inputs[i]);
             int error = label[i] - prediction;
             mse += error * error;
 
+            // update weight
             for (int j = 0; j < weight.size(); j++) {
                 weight[j] += learningrate * error * inputs[i][j];
             }
             bias += learningrate * error;
         }
 
+        lr -= learningrate;
+        // calculate the average and print for each epoch
         mse /= inputs.size();
         cout << "Epoch " << k + 1 << " MSE: " << mse << endl;
     }
